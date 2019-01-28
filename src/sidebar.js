a82a837283c483e1972cbdc539090de663d8f2b2
Derrick <ds777fighter@yahoo.com>
2019-01-28 11:22:55 -0500

    General improvements, added multi-layer

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,50 +1,96 @@
 L.Control.Sidebar = L.Control.extend({
 	options: {
 		position: 'topleft',
+		openOnAdd: false,
 	},
 	initialize: function(options) 
 	{
 		// Sets options of leaflet object
 		L.setOptions(this, options);
 		
-		this._isVisible = false;
-		
 		// Returns element from HTML based on its ID
 		this._sidebar = L.DomUtil.get('sidebar');
 		
-		L.DomUtil.addClass(this._sidebar, 'sidebar-left');
-		
-		if(L.Browser.touch) {
+		if(L.Browser.touch) 
+		{
 			L.DomUtil.addClass(this._sidebar, 'leaflet-touch');
 		}
-				
-		// Gets content for sidebar
-		this._content = this._sidebar.children[0];
+		
+		this._layers = [];
+		for(var i = 0; i < this._sidebar.children.length; i++)
+		{
+			this._layers.push(this._sidebar.children[i].innerHTML);
+		}
 	},
-	onAdd: function() {		
-		var div = L.DomUtil.create('div', 'command');
-		div.innerHTML = this._content.innerHTML;
-		div.id = "sidebar-interact";
-		div.style = "display: none;";
-		return div;
+	onAdd: function(map) 
+	{
+		if(this.options.position === 'topright' || this.options.position === 'bottomright')
+		{
+			if(map.attributionControl)
+			{
+				map.attributionControl.setPosition('bottomleft');
+			}
+		}
+		
+		this._container = L.DomUtil.create('div', 'leaflet-control');
+		this._container.innerHTML = this._layers[0];
+		this._container.id = "sidebar-interact";
+		
+		this._isVisible = this.options.openOnAdd;
+
+		if(this._isVisible)
+		{
+			this._container.style = "display: block;";
+		}
+		else
+		{
+			this._container.style = "display: none;";
+		}
+		
+		return this._container;
 	},
-	open: function() {
-		if(!this._isVisible) {
+	open: function() 
+	{
+		if(!this._isVisible) 
+		{
 			this._isVisible = true;
 			L.DomUtil.get("sidebar-interact").style = "display: block;";
 		}
 	},
-	close: function() {
-		if(this._isVisible) {
+	close: function() 
+	{
+		if(this._isVisible) 
+		{
 			this._isVisible = false;
 			L.DomUtil.get("sidebar-interact").style = "display: none;";
 		}
 	},
-	toggle: function() {
+	showLayer: function(index) 
+	{
+		if(index > this._layers.length)
+		{
+			return;
+		}
+			
+		while(this._container.firstChild)
+		{
+			this._container.removeChild(this._container.firstChild);
+		}
+		
+		this._container.innerHTML = this._layers[index];
+				
+		this._openLayer = index;
+	},
+	toggle: function() 
+	{
 		if(this._isVisible)
+		{
 			this.close();
+		}
 		else
+		{
 			this.open();
+		}
 	}
 	/* 
 		Possible functions:
